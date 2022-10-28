815210f4c3c844e9800c201f68a02af6cbe4cf11
Albert B <abduarte20@gmail.com>
2022-10-28 00:04:55 -0700

    1st commit

---
+++ Fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Albert Bravo");
+        System.out.println("I am 26 years old");
+        System.out.println("My hometown is Mountain View, CA");
+    }
+}

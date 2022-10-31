e942b6ee7534273c3a53a403e2a82f6ece5d1a0d
Albert B <abduarte20@gmail.com>
2022-10-30 19:26:17 -0700

    1st commit

---
+++ practice/week1/Modularization/Greetings.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+class Greetings {
+    public String getCurrentDate() {
+        Date date = new Date();
+        return "Current date is " + date;
+    }
+}
---
+++ practice/week1/Modularization/TestGreetings.java
@@ -0,0 +1,7 @@
+class TestGreetings {
+    public static void main(String args[]) {
+        Greetings instance1 = new Greetings();
+        String message = instance1.getCurrentDate();
+        System.out.println(message);
+    }
+}

d72600a847889b6d93ac11b3186aa807d595d9f0
Albert B <abduarte20@gmail.com>
2022-11-09 00:14:27 -0800

    1st commit

---
+++ Spring/Assignments/daikichiRoutes/src/test/java/com/albertbravo/daikichiroutes/DaikichiRoutesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertbravo.daikichiroutes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiRoutesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1198b54aab8c9f688f5ec9e552e92a21e5a8eec9
Albert B <abduarte20@gmail.com>
2022-11-13 22:53:09 -0800

    1st commit

---
+++ STSProjects/formDemo/src/main/java/com/albertbravo/formDemo/controllers/MainController.java
@@ -0,0 +1,32 @@
+package com.albertbravo.formDemo.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "index.jsp";
+	}
+	
+	@RequestMapping("/login")
+	public String login(
+			@RequestParam(value="email") String email,
+			@RequestParam(value="password") String password,
+			HttpSession session
+			) {
+		session.setAttribute("email", email);
+		return "redirect:/home";
+	}
+	
+	@RequestMapping("/home")
+	public String home() {
+		return "welcome.jsp";
+	}
+
+}
---
+++ STSProjects/formDemo/src/test/java/com/albertbravo/formDemo/FormDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertbravo.formDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

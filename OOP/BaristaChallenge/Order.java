db4dfe04aa487f2004c83ca9173f9bdc62f78b95
Albert B <abduarte20@gmail.com>
2022-11-29 01:16:38 -0800

    2nd commit

--- OOP/BaristaChallenge/Order.java
+++ OOP/BaristaChallenge/Order.java
@@ -10,17 +10,71 @@ public class Order {
     
     // CONSTRUCTOR
     // No arguments, sets name to "Guest", initializes items as an empty list.
+    public Order() {
+        this.name = "Guest";
+        this.items = new ArrayList<Item>();
+    }
     
     // OVERLOADED CONSTRUCTOR
     // Takes a name as an argument, sets name to this custom name.
     // Initializes items as an empty list.
+    public Order(String name) {
+        this.name = name;
+        this.items = new ArrayList<Item>();
+    }
     
     // ORDER METHODS
-    
-    	// Most of your code will go here, 
-    	// so implement the getters and setters first!
+    public void addItem(Item item) {
+        items.add(item);
+    }
+
+    public String getStatusMessage() {
+        if(ready) {
+            return "Your order is ready.";
+        } else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal() {
+        double total = 0;
+        for(int i=0; i<items.size(); i++) {
+            total += items.get(i).getPrice();
+        }
+        return total;
+    }
+
+    public void display() {
+        System.out.println("Customer Name: " + getName());
+        for(int i=0; i<items.size(); i++) {
+            System.out.println(items.get(i).getName() + " - $" + items.get(i).getPrice());
+        }
+        System.out.println("Total: $" + getOrderTotal());
+    }
     
     // GETTERS & SETTERS
-    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
 
--- OOP/BaristaChallenge/Test.java
+++ OOP/BaristaChallenge/Test.java
@@ -1,5 +1,30 @@
 public class Test {
     public static void main(String[] args) {
-        
+        // Menu items
+        Item item1 = new Item("mocha", 4.25);
+
+        Item item2 = new Item("latte", 4.05);
+
+        Item item3 = new Item("drip coffee", 3.50);
+
+        Item item4 = new Item("capuccino", 4.75);
+
+        // Orders
+        Order order1 = new Order();
+        Order order2 = new Order();
+        Order order3 = new Order("Cindy");
+        Order order4 = new Order("Brad");
+        Order order5 = new Order("Lana");
+        // System.out.println(order2.getName());
+
+        // Test
+        order1.addItem(item2);
+        order1.addItem(item3);
+        order1.display();
+        System.out.println(order1.getStatusMessage());
+        order1.setReady(true);
+        System.out.println(order1.getStatusMessage());
+        System.out.println(order1.getOrderTotal());
+
     }
 }

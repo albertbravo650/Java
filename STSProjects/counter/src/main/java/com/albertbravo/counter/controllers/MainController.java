172e3613fef44992e24072304c7940293182aa5b
Albert B <abduarte20@gmail.com>
2022-12-23 23:59:45 -0800

    commit

---
+++ STSProjects/counter/src/main/java/com/albertbravo/counter/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.albertbravo.counter.controllers;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	@GetMapping("/your_server")
+	public String index(HttpSession session) {
+		return "index.jsp";
+	}
+
+}

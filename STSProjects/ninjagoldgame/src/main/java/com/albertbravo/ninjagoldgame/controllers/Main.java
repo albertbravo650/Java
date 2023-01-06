6d28352d0b6ec8ffe3079114cdb674e51b6df9b4
Albert B <abduarte20@gmail.com>
2023-01-05 23:49:38 -0800

    next commit

--- STSProjects/ninjagoldgame/src/main/java/com/albertbravo/ninjagoldgame/controllers/Main.java
+++ STSProjects/ninjagoldgame/src/main/java/com/albertbravo/ninjagoldgame/controllers/Main.java
@@ -3,15 +3,15 @@ package com.albertbravo.ninjagoldgame.controllers;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Main {
 	
 	@SuppressWarnings("unchecked")
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String index(HttpSession session) {
 		if(session.getAttribute("gold") == null) {
 			session.setAttribute("gold", 0);
